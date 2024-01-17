2f846b2233cd8c296e754e7a79002d89e5e5f0f3
mjpediglorio <127216189+mjpediglorio@users.noreply.github.com>
2024-01-17 23:52:33 +0800

    Revised Working copy of Unit Tests

--- Rainfall ApiTests/Controllers/RainfallControllerTests.cs
+++ Rainfall ApiTests/Controllers/RainfallControllerTests.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Rainfall.Controllers;
 using System;
@@ -7,7 +8,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Rainfall.Api;
 
 namespace Rainfall.Controllers.Tests
 {
@@ -19,18 +19,32 @@ namespace Rainfall.Controllers.Tests
         {
             _sut = new RainfallController(new Api.Api());
         }
-
         [TestMethod()]
-        public void RainFallControllerTest_OK()
+        public void RainFallControllerTest_200()
         {
-            var result = _sut.Get(6830, 10);
-
-            var status_code = GetStatusCode(result.Result);
-
-            Assert.AreEqual(200, status_code, "Status code is not ok!");
+            var res = _sut.Get(3680, 10);
+            res.Wait();
+            var status_code = GetStatusCode(res.Result);
+            Assert.AreEqual(Microsoft.AspNetCore.Http.StatusCodes.Status200OK, status_code, "Status code is not ok!");
+        }
+        [TestMethod()]
+        public void RainFallControllerTest_400()
+        {
+            var res = _sut.Get(3680, 0);
+            res.Wait();
+            var status_code = GetStatusCode(res.Result);
+            Assert.AreEqual(Microsoft.AspNetCore.Http.StatusCodes.Status400BadRequest, status_code, "Status code is not ok!");
+        }
+        [TestMethod()]
+        public void RainFallControllerTest_404()
+        {
+            var res = _sut.Get(1, 10);
+            res.Wait();
+            var status_code = GetStatusCode(res.Result);
+            Assert.AreEqual(Microsoft.AspNetCore.Http.StatusCodes.Status404NotFound, status_code, "Status code is not ok!");
         }
 
-        private int? GetStatusCode<T>(ActionResult<T?> actionResult)
+        private static int? GetStatusCode<T>(ActionResult<T?> actionResult)
         {
             // source https://stackoverflow.com/questions/73594323/how-to-get-actionresult-statuscode-in-asp-net-core
             IConvertToActionResult convertToActionResult = actionResult; // ActionResult implicit implements IConvertToActionResult
